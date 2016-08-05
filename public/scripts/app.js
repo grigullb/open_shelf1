fbd01eded71aa946ca3ec4d2abfcd9a584be18a0
Angus Lei <anguslei@Anguss-MacBook-Pro.local>
2016-08-05 14:54:21 -0700

    ajax calls now reach the google books api, log to console book information in varaibles

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,21 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  $.ajax({
+    url: "https://www.googleapis.com/books/v1/volumes?q=isbn:1408855925", 
+    method: "get",
+    dataType: "json",
+    success: function (book_info)  {
+      console.log(book_info);
+      var results = book_info.items[0].volumeInfo;
+      var book_title = results.title;
+      var author = results.authors; //array
+      var page_count = results.pageCount;
+      var image = results.imageLinks.thumbnail; //smallThumbnail also available, both are URL links
+      var genre = results.categories[0]; //array 
+      console.log(book_title, author, page_count, genre);
+    }
+  });
+
 });
