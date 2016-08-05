1eb7bedd5f52978a80384992c03a5801972a075d
grigullb <vagrant@lighthouse-labs>
2016-08-05 20:01:25 +0000

    added books to seeds

---
+++ db/seeds/books.js
@@ -0,0 +1,9 @@
+exports.seed = function(knex, Promise) {
+    return knex('books').del()
+    .then(function () {
+      return Promise.all([
+        knex('books').insert({id: 1, title: 'Design is a Job', author_firstname: 'Mike', author_lastname: 'Monteiro', isbn: '9781937557041', user_id: 1}),
+        knex('books').insert({id: 2, title: 'On Web Typography', author_firstname: 'Jason', author_lastname: 'Santa Maria', isbn: '9781937557065', user_id: 2})
+      ]);
+    });
+};
