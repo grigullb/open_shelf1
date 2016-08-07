ece4565d99a3b02d34e684e0812b35dafcb96aae
Angus Lei <anguslei@Anguss-MacBook-Pro.local>
2016-08-06 22:05:53 -0700

    testing changes, database won't seed properly

--- db/seeds/books.js
+++ db/seeds/books.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex, Promise) {
-    return knex('books').del()
-    .then(function () {
+    // return knex('books').del()
+    // .then(function () {
       return Promise.all([
         knex('books').insert({id: 1, title: 'Design is a Job', author_firstname: 'Mike', author_lastname: 'Monteiro', isbn: '9781937557041', user_id: 1}),
         knex('books').insert({id: 2, title: 'On Web Typography', author_firstname: 'Jason', author_lastname: 'Santa Maria', isbn: '9781937557065', user_id: 2}),
         knex('books').insert({id: 3, title: 'Harry Potter and the Goblet of Fire', author_firstname: 'Jason', author_lastname: 'Santa Maria', isbn: '1408855925', user_id: 2})
       ]);
-    });
+    // });
 };
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,53 +11,53 @@ $(() => {
   });
 
   $(".show_books").on("click", ".book_link",function(event){
-        event.preventDefault();
-        var this_book_id = $(this).data("bookid");
-        $.ajax({
-          method: "GET",
-          url: "/api/books/"+this_book_id
-        }).done((book) => {
-            var this_isbn = book[0].isbn;
-            $.ajax({
-            url: "https://www.googleapis.com/books/v1/volumes?q=isbn:"+this_isbn, 
-            method: "get",
-            dataType: "json",
-            success: function (book_info)  {
-              var results = book_info.items[0].volumeInfo;
-              var book_title = results.title;
-              var author = results.authors; //array
-              var page_count = results.pageCount;
-              var genre = results.categories[0];
-              if (results.imageLinks){
-                var image = results.imageLinks.thumbnail
-              }  //smallThumbnail also available, both are URL links
-              var genre = results.categories[0]; //array 
-              if($('div').filter('[data-bookid="'+this_book_id+'"]').is(':empty')){
-                $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>'+book_title+'</p>');
-                if (author){
-                $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>'+author+'</p>');
-                }
-                if(image){
-                  $('div').filter('[data-bookid="'+this_book_id+'"]').append('<img src="'+image+'">');
-                } else{
-                  $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>&nbsp no image</p>');
-                }
-                $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>Genre: '+genre+'</p>');
-                if(page_count){
-                $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>Pages: '+page_count+'</p>');
-                }
-              } else{
-                $('div').filter('[data-bookid="'+this_book_id+'"]').empty();
-              }
-            } 
-          });
+    event.preventDefault();
+    var this_book_id = $(this).data("bookid");
+    $.ajax({
+      method: "GET",
+      url: "/api/books/"+this_book_id
+    }).done((book) => {
+      var this_isbn = book[0].isbn;
+      $.ajax({
+        url: "https://www.googleapis.com/books/v1/volumes?q=isbn:"+this_isbn, 
+        method: "get",
+        dataType: "json",
+        success: function (book_info)  {
+          var results = book_info.items[0].volumeInfo;
+          var book_title = results.title;
+          var author = results.authors; //array
+          var page_count = results.pageCount;
+          var genre = results.categories[0];
+          if (results.imageLinks){
+            var image = results.imageLinks.thumbnail
+          }  //smallThumbnail also available, both are URL links
+          var genre = results.categories[0]; //array 
+          if($('div').filter('[data-bookid="'+this_book_id+'"]').is(':empty')){
+            $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>'+book_title+'</p>');
+            if (author){
+            $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>'+author+'</p>');
+            }
+            if(image){
+              $('div').filter('[data-bookid="'+this_book_id+'"]').append('<img src="'+image+'">');
+            } else{
+              $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>&nbsp no image</p>');
+            }
+            $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>Genre: '+genre+'</p>');
+            if(page_count){
+              $('div').filter('[data-bookid="'+this_book_id+'"]').append('<p>Pages: '+page_count+'</p>');
+            }
+          } else {
+            $('div').filter('[data-bookid="'+this_book_id+'"]').empty();
+          }
+        } 
       });
+    });
   });
   
   
 
 
-$.ajax({
+  $.ajax({
     method: "GET",
     url: "/api/users/" + userId
   }).done((users) => {
@@ -67,5 +67,4 @@ $.ajax({
     }
   });
   
-
 });
