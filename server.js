3898105bbee10ea2e244df62a7d61154998af4d7
Angus Lei <anguslei@Anguss-MacBook-Pro.local>
2016-08-08 14:30:00 -0700

    added passport and added session requirements, testing to implement session functionality

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const PORT        = process.env.PORT || 3000;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const exphbs      = require('express-handlebars');
+const passport    = require('passport');
+const session     = require('express-session');
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
 const app         = express();
-
-
+  
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
@@ -28,6 +29,32 @@ app.use(morgan('dev'));
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
+// Cookie session requirements for passport
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+})); 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Passport strategy - Local (is this still needed necessarily)
+passport.use(new LocalStrategy(
+  function(email, password, done) {
+    User.findOne({email: email}, function(err, user){
+      if err { return done(err); };
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
+
+
+
+// Views and page display
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -63,10 +90,27 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.post("/login", (req, res) =>{       //this is calling a function, correct? 
+  User.findOne({ email: req.body.email }, function(err, user) {
+    if (!user) {
+      res.render('login', { error: 'Invalid email or password.' });
+    } else {
+      if (req.body.password === user.password) {
+        //this should set the cookie session with the user info itself 
+        req.session.user = user;
+        res.redirect('/dashboard');
+      } else {
+        res.render('login', { error: 'Invalid email or password.' });
+      }
+    }
+  });
+});
+
 //New Book Submission
 app.get("/new", (req, res) => {
   res.render("submit");
 });
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
