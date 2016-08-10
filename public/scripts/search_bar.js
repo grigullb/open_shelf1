810abf85aca9cbedacd711e193dfbb97cb0c8af0
grigullb <vagrant@lighthouse-labs>
2016-08-10 23:09:56 +0000

    searching brings up book info and message option, but messaging not setup

--- public/scripts/search_bar.js
+++ public/scripts/search_bar.js
@@ -19,36 +19,67 @@ $(() => {
     $(".is-success").on("click",function(){
         runSearch(search_term);
     });
+
     $(document).on("click", ".book_select",function(event){
-      $('#search_book_info').remove();
-      $('<div id="msg_overlay">').css({
-      "width" : "100%"
-    , "height" : "100%"
-    , "background" : "#000"
-    , "position" : "fixed"
-    , "top" : "0"
-    , "left" : "0"
-    , "z-index" : 2
-    , "MsFilter" : "progid:DXImageTransform.Microsoft.Alpha(Opacity=60)"
-    , "filter" : "alpha(opacity=60)"
-    , "MozOpacity" : 0.6
-    , "KhtmlOpacity" : 0.6
-    , "opacity" : 0.6
-    }).appendTo(document.body);
-    $('<div id="search_book_info" class="card">').css({
-      "width" : "50%"
-    , "height" : "50%"
-    , "background" : "#ffffff"
-    , "position" : "fixed"
-    , "top" : "50%"
-    , "left" : "50%"
-    , "z-index" : 3
-    ,'-webkit-transform': 'translate(-50%, -50%)'
-    }).appendTo(document.body);
-    });
+      var this_book_id = $(this).data("idbook");
+      addDim();
+      addPopup();
+      $.ajax({
+      method: "GET",
+      url: "/api/books/user_books/"+this_book_id
+      }).done((book) => {
+        var this_isbn = book[0].isbn;
+        $.ajax({
+          method: "GET",
+          url: "/api/users/books/"+this_book_id
+          }).done((users) => {
+            for(user of users){
+              var book_owner = user.firstname;
+              $.ajax({
+                url: "https://www.googleapis.com/books/v1/volumes?q=isbn:"+this_isbn, 
+                method: "get",
+                dataType: "json",
+                success: function (book_info)  {
+                  var results = book_info.items[0].volumeInfo;
+                  var book_title = results.title;
+                  var author = results.authors; //array
+                  var page_count = results.pageCount;
+                  var genre = results.categories[0];
+                  if (results.imageLinks){
+                    var image = results.imageLinks.thumbnail
+                  }  //smallThumbnail also available, both are URL links
+                  var genre = results.categories[0]; //array 
+                  if($('#search_book_info').is(':empty')){
+                    $('#search_book_info').append('<p>'+book_title+'</p>');
+                    if (author){
+                      $('#search_book_info').append('<p>'+author+'</p>');
+                    }
+                    if(image){
+                      $('#search_book_info').append('<img src="'+image+'">');
+                    } else{
+                      $('#search_book_info').append('<p>&nbsp no image</p>');
+                    }
+                    $('#search_book_info').append('<p>Genre: '+genre+'</p>');
+                    if(page_count){
+                      $('#search_book_info').append('<p>Pages: '+page_count+'</p>');
+                    }
+                    $('#search_book_info').append('<p><em>'+book_owner+'</em> is the owner of this Book. Message <em>'+book_owner+'</em> to show your interested in the book</p>');
+                    $('#search_book_info').append('<textarea></textarea>');
+                    $('#search_book_info').append('<button>Submit</button>');
+                  } else {
+                    $('#search_book_info').empty();
+                  }
+
+                 } 
+              });
+            }
+          });
+        });
+     });
+
+
     $(document).on("click", "#msg_overlay",function(event){
-      $('#msg_overlay').remove();
-      $('#search_book_info').css('position', 'absolute').css('top', '60%');
+      removeDim();
     });
 });
 
@@ -65,3 +96,40 @@ function runSearch(search_term){
         }
       });
 }
+function addDim(){
+  $('#search_book_info').remove();
+      $('<div id="msg_overlay">').css({
+      "width" : "100%"
+    , "height" : "100%"
+    , "background" : "#000"
+    , "position" : "fixed"
+    , "top" : "0"
+    , "left" : "0"
+    , "z-index" : 2
+    , "MsFilter" : "progid:DXImageTransform.Microsoft.Alpha(Opacity=60)"
+    , "filter" : "alpha(opacity=60)"
+    , "MozOpacity" : 0.6
+    , "KhtmlOpacity" : 0.6
+    , "opacity" : 0.6
+    }).appendTo(document.body);
+}
+
+function addPopup(){
+  $('<div id="search_book_info" class="card">').css({
+      "width" : "50%"
+    , "height" : "50%"
+    , "background" : "#ffffff"
+    , "position" : "fixed"
+    , "top" : "50%"
+    , "left" : "50%"
+    , "z-index" : 3
+    ,'-webkit-transform': 'translate(-50%, -50%)'
+    }).appendTo(document.body);
+}
+
+function removeDim(){
+  $('#msg_overlay').remove();
+  $('#search_book_info').css('position', 'absolute').css('top', '65%');
+}
+
+
