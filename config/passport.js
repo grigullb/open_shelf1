b1864f93cd12af8ff4c8aada7736610adf154484
Angus Lei <anguslei@Anguss-MacBook-Pro.local>
2016-08-10 12:52:14 -0700

    improved notes and documentation in passport.js

--- config/passport.js
+++ config/passport.js
@@ -4,42 +4,44 @@ const bcrypt  = require('bcrypt-nodejs');
 let User = require('../models/user');
 
 module.exports = function(passport) {
+  //bcrypt function to generate salted password
+  function generateHash(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+  }
   // passport session setup
-  // used to serialize the user for the session
   passport.serializeUser(function(user, done) {
+      console.log(user);
       done(null, user.id);
   });
 
-  // used to deserialize the user
   passport.deserializeUser(function(id, done) {
       //TODO: verify findById is a valid query (source is Mongo, Bookshelf equivalent?)
-      User.findById(id, function(err, user) {
+      // User.findById(id, function(err, user) {
+      //     done(err, user);
+      // });
+      User.where({ 'id':  id }).fetch().then( function(err, user) {
           done(err, user);
       });
   });
 
-  function generateHash(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-  }
-
-  // name the strategy for futureproofing other authentication methods 
+  // SIGNUP LOGIC########################
   passport.use('local-signup', new LocalStrategy({
-      // default local strategy uses username and password, override with email
+      // default local strategy uses username and password - override with email
       usernameField : 'email',
       passwordField : 'password',
-      passReqToCallback : true // allows passing back the entire request to the callback
+      passReqToCallback : true // allows passing the HTTP request into the callback 
   }, 
-  function(req, email, password, done) {  //is this from the form data?
+  // fields passed from the form input tag with a corresponding name
+  function(req, email, password, done) { 
       // asynchronous
       // User.where wont fire unless data is sent back
       process.nextTick(function() {
-      // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
+      // checks whether the user email already exists
       User.where({ 'email':  email }).fetch().then( function(user) {
           // check to see if theres already a user with that email
           if (user) {
               return done(null, false); 
-              // return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+              // TODO: flash messages. return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
           } else {
               // if there is no user with that email, create user
               User.forge({
@@ -50,35 +52,34 @@ module.exports = function(passport) {
                 password: generateHash(password),
               })
               .save()
+              //return the newUser object
               .then(function(newUser){
                 console.log(newUser);
                 return done(null, newUser);
               })
               .catch(function (err){
-                console.log(err);
+                console.log(err);     ////debugging
               });
           }
         });    
      });
   }));
 
+  // LOGIN LOGIC########################
   passport.use('local-login', new LocalStrategy({
       // by default, local strategy uses username and password, we will override with email
       usernameField : 'email',
       passwordField : 'password',
       passReqToCallback : true // allows us to pass back the entire request to the callback
   },
-  function(req, email, password, done) { // callback with email and password from our form
+  function(req, email, password, done) { 
       // find a user whose email is the same as the forms email
-      // we are checking to see if the user trying to login already exists
       User.where({ 'email':  email }).fetch().then( function(user) {
-          console.log(user);
           if (!user || !user.validPassword(password)) {
               return done(null, false);
-              // return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
-          }// all is well, return successful user
-          return done(null, user);
+              // TODO: flash message .return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+          }
+          return done(null, user); // all is well, return successful user
       });
-
   }));
 }
