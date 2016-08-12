66dd91756a6209fcb7bc1fa4f0f1c0ece1155d0c
grigullb <vagrant@lighthouse-labs>
2016-08-12 18:38:57 +0000

    descriptions are displayed

--- db/seeds/books.js
+++ db/seeds/books.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
     return knex('books').del()
     .then(function () {
       return Promise.all([
-        knex('books').insert({title: 'Content strategy for mobile', isbn: '9781937557041', user_id: 1, genre_id: 2, author_id: 1, condition: 'not book'}),
-        knex('books').insert({title: 'On Web Typography', isbn: '9781937557065', user_id: 2, genre_id: 2, condition: 'not', author_id: 2}),
-        knex('books').insert({title: 'Harry Potter and the Goblet of Fire', isbn: '1408855925', user_id: 1, genre_id: 1, condition: 'not good', author_id: 3})
+        knex('books').insert({title: 'content strategy for mobile', isbn: '9781937557041', user_id: 1, genre_id: 2, author_id: 1, condition: 'missing pages 100-200'}),
+        knex('books').insert({title: 'on web typography', isbn: '9781937557065', user_id: 2, genre_id: 2, condition: 'Brand spanking new.', author_id: 2}),
+        knex('books').insert({title: 'harry potter and the goblet of fire', isbn: '1408855925', user_id: 1, genre_id: 1, condition: 'Brand spanking old', author_id: 3})
       ]);
     });
 };
