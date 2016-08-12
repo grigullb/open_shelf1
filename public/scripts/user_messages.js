ff28d70b2cb6be19567317de665e0e80bfb513e4
Angus Lei <anguslei@Anguss-MacBook-Pro.local>
2016-08-12 16:51:55 -0700

    refactoring message javascript for future feature

--- public/scripts/user_messages.js
+++ public/scripts/user_messages.js
@@ -1,15 +1,15 @@
 $(() => {
 	var userId = $('#user-id').val();
-	getMessages(userId);
+	updateMessageDisplay(userId);
   $('#message_box').on('click', function(){
   	addDim();
   	addPopup();
   	$.ajax({
-    method: "GET",
-    url: "/api/messages/"+userId
+      method: "GET",
+      url: "/api/messages/"+userId
   	}).done((messages) => {
-  		if(messages.length!==0){
-  			var message_count = 1;
+  		if(messages.length > 0){
+  			var message_count = messages.length;
 		    for(mes of messages) {
 			    $('.panel-tabs').after('<a class="panel-block is-active" href="#"> \
 			   	<span class="panel-icon"> \
@@ -32,9 +32,35 @@ $(() => {
   });
 });
 
+function updateMessageDisplay(userId){
+  $.ajax({
+    method: "GET",
+    url: "/api/messages/"+userId
+  }).done((messages) => {
+    var messageSingular = messages.length;
+    $('#message_count').text(messageSingular);
+    if(messageSingular===1){
+      $('#message_header').text('Message');
+    } else{
+      $('#message_header').text('Messages');
+    }
+  });
+}
+
+function getUserName(userId, message_count){
+  $.ajax({
+    method: "GET",
+    url: "/api/users/" + userId
+  }).done((users) => {
+    for(user of users) {
+      $('span').filter('[data-count="'+message_count+'"]').text('From '+user.firstname+': ');
+    }
+  });
+}
+
 function addDim(){
   $('#search_book_info').remove();
-      $('<div id="msg_overlay">').css({
+  $('<div id="msg_overlay">').css({
       "width" : "100%"
     , "height" : "100%"
     , "background" : "#000"
@@ -76,31 +102,3 @@ function addPopup(){
     }).appendTo(document.body);
 }
 
-function getMessages(userId){
-	$.ajax({
-    method: "GET",
-    url: "/api/messages/"+userId
-  }).done((messages) => {
-  	var messageTotal = 0;
-    for(mes of messages) {
-      console.log(mes);
-      messageTotal ++;
-    }
-    $('#message_count').text(messageTotal);
-    if(messageTotal===1){
-    	$('#message_header').text('Message');
-  	}else{
-  		$('#message_header').text('Messages');
-  	}
-  });
-}
-function getUserName(userId, message_count){
-	$.ajax({
-	    method: "GET",
-	    url: "/api/users/" + userId
-	  }).done((users) => {
-	    for(user of users) {
-	    	$('span').filter('[data-count="'+message_count+'"]').text('From '+user.firstname+': ');
-	  }
-	});
-}
